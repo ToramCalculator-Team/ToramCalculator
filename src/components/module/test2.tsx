43315d7bf8ab0189f90431060f3c600c335d28ee
KiaClouth <mayunlong16@foxmail.com>
2024-11-15 17:43:23 +0800

    fix: 隐藏inspector

--- src/components/module/test2.tsx
+++ src/components/module/test2.tsx
@@ -13,7 +13,7 @@ import { SceneLoader } from "@babylonjs/core/Loading/sceneLoader";
 import "@babylonjs/loaders/glTF/2.0/glTFLoader";
 import * as _ from "lodash-es";
 // import "@babylonjs/core/Debug/debugLayer"; // Augments the scene with the debug methods
-import "@babylonjs/inspector"; // Injects a local ES6 version of the inspector to prevent automatically relying on the none compatible version
+// import "@babylonjs/inspector"; // Injects a local ES6 version of the inspector to prevent automatically relying on the none compatible version
 import { SpotLight } from "@babylonjs/core/Lights/spotLight";
 import { UniversalCamera } from "@babylonjs/core/Cameras/universalCamera";
 import { MeshBuilder } from "@babylonjs/core/Meshes/meshBuilder";
