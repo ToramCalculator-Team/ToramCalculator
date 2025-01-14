4792b94b27a41f8e7219b627e6f6007cc31335f5
KiaClouth <clouthber@gmail.com>
2025-01-14 14:51:18 +0800

    feat:pwa提示测试

--- src/routes/(app).tsx
+++ src/routes/(app).tsx
@@ -2,19 +2,27 @@ import { ParentProps, Show } from "solid-js";
 import { Motion } from "solid-motionone";
 import { store } from "~/store";
 import Setting from "~/components/module/setting-page";
-import BabylonBg from "~/components/module/test2";
+// import BabylonBg from "~/components/module/test2";
 import RandomBallBackground from "~/components/module/randomBallBg";
 
 export default function AppMainContet(props: ParentProps) {
+  // pwa安装提示
+  let deferredPrompt;
+  window.addEventListener("beforeinstallprompt", (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    console.log("beforeinstallprompt");
+  });
+
   return (
     <>
-      <Show when={store.settings.userInterface.is3DbackgroundDisabled}>
+      {/* <Show when={store.settings.userInterface.is3DbackgroundDisabled}>
         <BabylonBg />
-      </Show>
+      </Show> */}
       <RandomBallBackground />
       <Motion.div
         id="AppMainContet"
-        class={`h-dvh w-dvw overflow-hidden ${store.settingsDialogState ? "scale-[95%] blur-sm opacity-0" : "scale-100 blur-0 opacity-100"}`}
+        class={`h-dvh w-dvw overflow-hidden ${store.settingsDialogState ? "scale-[95%] opacity-0 blur-sm" : "scale-100 opacity-100 blur-0"}`}
       >
         {props.children}
       </Motion.div>
